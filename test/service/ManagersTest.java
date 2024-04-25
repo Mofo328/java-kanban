dc0e91688a9000b9862c10e8c806a7ccc441e349
Mofo328 <163290172+Mofo328@users.noreply.github.com>
2024-04-25 12:29:03 +0500

    шестой коммит "исправил все таки equals"

--- test/service/ManagersTest.java
+++ test/service/ManagersTest.java
@@ -1,25 +1,16 @@
 package service;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagersTest {
 
-    private TaskManager taskManager;
-    private HistoryManager historyManager;
-
-    @BeforeEach
-    public void BeforeEach() {
-        taskManager = Managers.getDefault();
-        historyManager = Managers.getDefaultHistory();
-    }
-
     @Test
     public void shouldManagerCanReturnManagersReadyToWork() {
-        assertNotNull(taskManager);
-        assertNotNull(historyManager);
+        assertNotNull(Managers.getDefault());
+        assertNotNull(Managers.getDefaultHistory());
     }
 
 }
